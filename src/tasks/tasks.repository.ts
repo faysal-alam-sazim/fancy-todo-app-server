2d21821aa6782a3b9fce97f3d859dce26981d547
Faysal Alam <faysal.alam@sazim.io>
2024-08-04 00:17:06 +0600

    feat(FTDAS): Create POST /todos api to create task

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -1,7 +1,11 @@
 import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
 import { Task } from 'src/common/entities/task.entity';
+<<<<<<< HEAD
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
+=======
+import { CreateTaskDto } from './tasks.dto';
+>>>>>>> 4d567fc (feat(FTDAS): Create POST /todos api to create task)
 
 @Injectable()
 export class TasksRepository extends EntityRepository<Task> {
@@ -17,6 +21,12 @@ export class TasksRepository extends EntityRepository<Task> {
 
   async updateOne(task: Task, updateTaskDto: UpdateTaskDto): Promise<Task> {
     this.assign(task, updateTaskDto);
+    await this.em.persistAndFlush(task);
+    return task;
+  }
+
+  async createOne(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.create(createTaskDto);
     await this.em.persistAndFlush(task);
     return task;
   }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Task } from 'src/common/entities/task.entity';
+<<<<<<< HEAD
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
 import { TasksRepository } from './tasks.repository';
 
@@ -19,5 +20,20 @@ export class TasksService {
   async updateOne(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
     const task = await this.tasksRepository.findOneOrFail(id);
     return await this.tasksRepository.updateOne(task, updateTaskDto);
+=======
+import { CreateTaskDto } from './tasks.dto';
+import { TaskRepository } from './tasks.repository';
+
+@Injectable()
+export class TasksService {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  async findAll(): Promise<Task[]> {
+    return await this.taskRepository.findAll();
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return await this.taskRepository.createOne(createTaskDto);
+>>>>>>> 4d567fc (feat(FTDAS): Create POST /todos api to create task)
   }
 }
