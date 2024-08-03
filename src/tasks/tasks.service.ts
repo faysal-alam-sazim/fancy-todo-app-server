dc647994d118e47ca3baa674c3444ae9b43cb620
Faysal Alam <faysal.alam@sazim.io>
2024-08-04 00:17:06 +0600

    feat(FTDAS): Create POST /todos api to create task

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/postgresql';
+import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 
 import { Task } from 'src/common/entities/task.entity';
+import { CreateTaskDto } from './tasks.dto';
 
 @Injectable()
 export class TasksService {
   constructor(
     @InjectRepository(Task)
     private readonly taskRepository: EntityRepository<Task>,
+    private readonly em: EntityManager,
   ) {}
+
   async findAll(): Promise<Task[]> {
-    return this.taskRepository.findAll();
+    return await this.taskRepository.findAll();
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.taskRepository.create(createTaskDto);
+    await this.em.persistAndFlush(task);
+    return task;
   }
 }
