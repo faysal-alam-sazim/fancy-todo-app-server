85a9df720966eef8cfa690910f0ac91a8605082e
Faysal Alam <faysal.alam@sazim.io>
2024-08-04 20:51:37 +0600

    feat(FTDAS): Add Delete /todos/completed to delete completed tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -55,7 +55,12 @@ export class TasksController {
   }
 
   @Get(':id')
-  async findOneTask(@Param('id') id: string): Promise<Task> {
-    return this.tasksService.findOneTask(+id);
+  async findOne(@Param('id') id: string): Promise<Task> {
+    return this.tasksService.findOne(+id);
+  }
+
+  @Delete('completed')
+  async removeCompleted(): Promise<void> {
+    await this.tasksService.removeCompleted();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,6 +9,7 @@ import { Injectable } from '@nestjs/common';
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 import { Task } from 'src/common/entities/task.entity';
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
@@ -72,6 +73,8 @@ import { TaskRepository } from './tasks.repository';
 =======
 import { InjectRepository } from '@mikro-orm/nestjs';
 import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
+=======
+>>>>>>> 3e90812 (feat(FTDAS): Add Delete /todos/completed to delete completed tasks)
 
 import { Task } from 'src/common/entities/task.entity';
 <<<<<<< HEAD
@@ -82,6 +85,7 @@ import { CreateTaskDto } from './tasks.dto';
 =======
 import { Task } from 'src/common/entities/task.entity';
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
+<<<<<<< HEAD
 import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 import { TaskRepository } from './tasks.repository';
 >>>>>>> 57d0b96 (feat(FTDAS): Create PUT /todos/:id to update task)
@@ -214,20 +218,40 @@ export class TasksService {
     await this.em.persistAndFlush(task);
     return task;
 >>>>>>> dc64799 (feat(FTDAS): Create POST /todos api to create task)
+=======
+import { TasksRepository } from './tasks.repository';
+
+@Injectable()
+export class TasksService {
+  constructor(private readonly tasksRepository: TasksRepository) {}
+
+  async findAll(): Promise<Task[]> {
+    return await this.tasksRepository.findAll();
+  }
+
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return await this.tasksRepository.createOne(createTaskDto);
+>>>>>>> 3e90812 (feat(FTDAS): Add Delete /todos/completed to delete completed tasks)
   }
 
   async updateTask(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
-    return await this.taskRepository.updateTask(id, updateTaskDto);
+    const task = await this.tasksRepository.findOneOrFail(id);
+    return await this.tasksRepository.updateOne(task, updateTaskDto);
   }
 
   async deleteTask(id: number) {
-    const task = await this.taskRepository.findOne(id);
-    if (task) {
-      await this.em.removeAndFlush(task);
-    }
+    const task = await this.tasksRepository.findOne(id);
+    return await this.tasksRepository.deleteOne(task);
   }
 
-  async findOneTask(id: number): Promise<Task> {
-    return await this.taskRepository.findOneOrFail(id);
+  async findOne(id: number): Promise<Task> {
+    return await this.tasksRepository.findOneOrFail(id);
+  }
+
+  async removeCompleted(): Promise<void> {
+    const completedTasks = await this.tasksRepository.find({
+      status: 'completed',
+    });
+    await this.tasksRepository.removeCompleted(completedTasks);
   }
 }
