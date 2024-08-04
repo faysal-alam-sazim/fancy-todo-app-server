16645eeeda0aa361e89a0f783e841fb77b73f90d
Faysal Alam <faysal.alam@sazim.io>
2024-08-04 12:59:39 +0600

    feat(FTDAS): Add Delete /todos/:id api and delete functionality

--- src/tasks/tasks.repository.ts
+++ src/tasks/tasks.repository.ts
@@ -16,6 +16,21 @@ export class TasksRepository extends EntityRepository<Task> {
   constructor(em: EntityManager) {
     super(em, Task);
   }
+<<<<<<< HEAD
+
+  async createOne(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = this.create(createTaskDto);
+    await this.em.persistAndFlush(task);
+    return task;
+  }
+
+  async updateOne(task: Task, updateTaskDto: UpdateTaskDto): Promise<Task> {
+    this.assign(task, updateTaskDto);
+    await this.em.persistAndFlush(task);
+    return task;
+  }
+=======
+>>>>>>> e54b1e0 (feat(FTDAS): Add Delete /todos/:id api and delete functionality)
 
   async createOne(createTaskDto: CreateTaskDto): Promise<Task> {
     const task = this.create(createTaskDto);
@@ -29,15 +44,7 @@ export class TasksRepository extends EntityRepository<Task> {
     return task;
   }
 
-  async createOne(createTaskDto: CreateTaskDto): Promise<Task> {
-    const task = this.create(createTaskDto);
-    await this.em.persistAndFlush(task);
-    return task;
-  }
-
-  async updateOne(task: Task, updateTaskDto: UpdateTaskDto): Promise<Task> {
-    this.assign(task, updateTaskDto);
-    await this.em.persistAndFlush(task);
-    return task;
+  async deleteOne(task: Task) {
+    await this.em.removeAndFlush(task);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
+
 import { Task } from 'src/common/entities/task.entity';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
 import { TasksRepository } from './tasks.repository';
 
@@ -10,7 +9,6 @@ export class TasksService {
   constructor(private readonly tasksRepository: TasksRepository) {}
 
   async findAll(): Promise<Task[]> {
-    return await this.tasksRepository.findAll();
     return await this.tasksRepository.findAll();
   }
 
@@ -21,28 +19,10 @@ export class TasksService {
   async updateOne(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
     const task = await this.tasksRepository.findOneOrFail(id);
     return await this.tasksRepository.updateOne(task, updateTaskDto);
-=======
-import { CreateTaskDto } from './tasks.dto';
-=======
-import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
->>>>>>> e018994 (feat(FTDAS): Create PUT /todos/:id to update task)
-import { TaskRepository } from './tasks.repository';
-
-@Injectable()
-export class TasksService {
-  constructor(private readonly taskRepository: TaskRepository) {}
-
-  async findAll(): Promise<Task[]> {
-    return await this.taskRepository.findAll();
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    return await this.taskRepository.createOne(createTaskDto);
->>>>>>> 4d567fc (feat(FTDAS): Create POST /todos api to create task)
-  }
-
-  async updateTask(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
-    const task = await this.taskRepository.findOneOrFail(id);
-    return await this.taskRepository.updateOne(task, updateTaskDto);
+  async deleteOne(id: number) {
+    const task = await this.tasksRepository.findOne(id);
+    return await this.tasksRepository.deleteOne(task);
   }
 }
