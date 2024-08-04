20b9dc944487b73e25b7a668afa8883c370d476f
Faysal Alam <faysal.alam@sazim.io>
2024-08-04 20:51:37 +0600

    feat(FTDAS): Add Delete /todos/completed to delete completed tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 31ac275 (feat(FTDAS): Add Delete /todos/:id api and delete functionality)
 import {
   Body,
   Controller,
@@ -13,37 +7,10 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-<<<<<<< HEAD
 
 import { TasksService } from './tasks.service';
 import { Task } from 'src/common/entities/task.entity';
 import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
-=======
-import { Body, Controller, Get, Post } from '@nestjs/common';
-
-=======
-import {
-  Body,
-  Controller,
-  Get,
-  InternalServerErrorException,
-  Post,
-} from '@nestjs/common';
->>>>>>> 50fe548 (feat(FTDAS): Create POST /todos api to create task)
-import { TasksService } from './tasks.service';
-import { Task } from 'src/common/entities/task.entity';
-
-import { CreateTaskDto } from './tasks.dto';
->>>>>>> dc64799 (feat(FTDAS): Create POST /todos api to create task)
-=======
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
-=======
->>>>>>> 31ac275 (feat(FTDAS): Add Delete /todos/:id api and delete functionality)
-
-import { TasksService } from './tasks.service';
-import { Task } from 'src/common/entities/task.entity';
-import { CreateTaskDto, UpdateTaskDto } from './tasks.dto';
->>>>>>> 57d0b96 (feat(FTDAS): Create PUT /todos/:id to update task)
 
 @Controller('todos')
 export class TasksController {
@@ -55,7 +22,6 @@ export class TasksController {
   }
 
   @Post()
-<<<<<<< HEAD
   async createOne(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return await this.tasksService.createOne(createTaskDto);
   }
@@ -66,14 +32,11 @@ export class TasksController {
     @Body() updateTaskDto: UpdateTaskDto,
   ): Promise<Task> {
     return this.tasksService.updateOne(+id, updateTaskDto);
-<<<<<<< HEAD
   }
 
-  @Delete(':id')
-  async deleteOne(@Param('id') id: string) {
-    return await this.tasksService.deleteOne(+id);
-=======
->>>>>>> 3c04e04 (feat(FTDAS): Add Get /todos/:id API to fetch one task)
+  @Delete('completed')
+  async removeCompleted(): Promise<void> {
+    await this.tasksService.removeCompleted();
   }
 
   @Delete(':id')
@@ -84,27 +47,5 @@ export class TasksController {
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Task> {
     return this.tasksService.findOne(+id);
-=======
-  async createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-    return await this.tasksService.createTask(createTaskDto);
->>>>>>> dc64799 (feat(FTDAS): Create POST /todos api to create task)
-  }
-
-  @Put(':id')
-  async updateTask(
-    @Param('id') id: string,
-    @Body() updateTaskDto: UpdateTaskDto,
-  ): Promise<Task> {
-    return this.tasksService.updateTask(+id, updateTaskDto);
-  }
-
-  @Delete(':id')
-  async deleteTask(@Param('id') id: string) {
-    return await this.tasksService.deleteTask(+id);
-  }
-
-  @Get(':id')
-  async findOneTask(@Param('id') id: string): Promise<Task> {
-    return this.tasksService.findOneTask(+id);
   }
 }
