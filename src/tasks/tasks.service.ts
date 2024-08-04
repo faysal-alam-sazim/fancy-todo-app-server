17b6197810f9d4ce7ecd1bf84442a94e51782ae0
Faysal Alam <faysal.alam@sazim.io>
2024-08-04 12:59:39 +0600

    feat(FTDAS): Add Delete /todos/:id api and delete functionality

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -307,4 +307,11 @@ export class TasksService {
       await this.em.removeAndFlush(task);
     }
   }
+
+  async deleteTask(id: number) {
+    const task = await this.taskRepository.findOne(id);
+    if (task) {
+      await this.em.removeAndFlush(task);
+    }
+  }
 }
