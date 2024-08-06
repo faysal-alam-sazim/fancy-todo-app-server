28c5cac07bc862c1e9ee3c244d913ec3a6b8c350
Faysal Alam <faysal.alam@sazim.io>
2024-08-06 17:25:44 +0600

    chore(FTDA): Enbale CORS for localhost:3000

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -5,18 +5,26 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { Task } from 'src/common/entities/task.entity';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { TasksRepository } from './tasks.repository';
 =======
 import { TaskRepository } from './tasks.repository';
 >>>>>>> 57d0b96 (feat(FTDAS): Create PUT /todos/:id to update task)
+=======
+import { TasksRepository } from './tasks.repository';
+>>>>>>> 86bb982 (chore(FTDA): Enbale CORS for localhost:3000)
 
 @Module({
   imports: [MikroOrmModule.forFeature([Task])],
   controllers: [TasksController],
 <<<<<<< HEAD
+<<<<<<< HEAD
   providers: [TasksService, TasksRepository],
 =======
   providers: [TasksService, TaskRepository],
 >>>>>>> 57d0b96 (feat(FTDAS): Create PUT /todos/:id to update task)
+=======
+  providers: [TasksService, TasksRepository],
+>>>>>>> 86bb982 (chore(FTDA): Enbale CORS for localhost:3000)
 })
 export class TasksModule {}
